b3d3e3e3680db31088997748474edf17418c55d5
forhadmia1 <forhad8613@gmail.com>
2025-03-28 01:20:48 +0600

    full screen done

--- src/pages/LiveStream.tsx
+++ src/pages/LiveStream.tsx
@@ -193,15 +193,61 @@ const LiveStream = () => {
     };
   }, [location]);
 
+  const handleFullScreen = () => {
+    if (canvasRef.current.requestFullscreen) {
+      canvasRef.current.requestFullscreen();
+    }
+  };
+
   return (
     <div className="flex">
       <div className="basis-2/3">
-        <div className="w-full h-[70vh]">
+        <div className="w-full h-[70vh] relative">
           <canvas
             ref={canvasRef}
             className="w-full h-full"
             style={{ border: '2px solid black' }}
           />
+          <div
+            onClick={handleFullScreen}
+            className="absolute top-[90%] right-5 cursor-pointer"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="#000000"
+              height="24px"
+              width="24px"
+              version="1.1"
+              id="Layer_1"
+              viewBox="0 0 512 512"
+            >
+              <g>
+                <g>
+                  <path d="M0,0v512h512V0H0z M477.867,477.867H34.133V34.133h443.733V477.867z" />
+                </g>
+              </g>
+              <g>
+                <g>
+                  <polygon points="126.533,102.4 199.111,102.4 199.111,68.267 68.267,68.267 68.267,199.111 102.4,199.111 102.4,126.538     198.422,222.558 222.556,198.423   " />
+                </g>
+              </g>
+              <g>
+                <g>
+                  <polygon points="222.557,313.581 198.422,289.445 102.4,385.467 102.4,312.889 68.267,312.889 68.267,443.733 199.111,443.733     199.111,409.6 126.538,409.6   " />
+                </g>
+              </g>
+              <g>
+                <g>
+                  <polygon points="409.6,312.889 409.6,385.467 313.578,289.444 289.444,313.578 385.462,409.6 312.889,409.6 312.889,443.733     443.733,443.733 443.733,312.889   " />
+                </g>
+              </g>
+              <g>
+                <g>
+                  <polygon points="312.889,68.267 312.889,102.4 385.467,102.4 289.444,198.423 313.578,222.558 409.6,126.538 409.6,199.111     443.733,199.111 443.733,68.267   " />
+                </g>
+              </g>
+            </svg>
+          </div>
         </div>
         {!isStreaming ? (
           <button
